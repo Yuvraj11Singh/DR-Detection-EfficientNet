85f4d96ccd0676f200a928420ee5796560fbe9a4
yuvrajsingh <yuvrajsinghmuj@gmail.com>
2026-04-29 19:53:34 +0530

    Fixed the loading error

--- script.js
+++ script.js
@@ -929,3 +929,9 @@ function clearAllData() {
 document.querySelectorAll('a[href^="#"]').forEach(a => {
   a.addEventListener('click', e => { e.preventDefault(); document.querySelector(a.getAttribute('href'))?.scrollIntoView({ behavior: 'smooth' }); });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadSettingsUI();
+  initLabUpload();
+  renderNotesList();
+});
